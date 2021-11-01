8638d8f957ea4aca89299dc3247e1eaa64adc3d8
Vinit <vinitkothari28@gmail.com>
2021-11-01 19:32:21 -0400

    pproblem_done

--- pProblem.py
+++ pProblem.py
@@ -1,11 +1,11 @@
 import pandas as pd
 
-df = pd.read_csv('early.csv')
+df = pd.read_csv('S19_Release_6_28_21.zip/Train/early.csv')
 
 df.insert(4, 'pProblemSyntaxError', range(len(df)))
 df.insert(5, 'pProblemSemanticError', range(len(df)))
 
-df2 = pd.read_csv('MainTable.csv')
+df2 = pd.read_csv('S19_Release_6_28_21.zip/Train/Data/MainTable.csv')
 
 
 
@@ -25,6 +25,35 @@ def getSubjects(df):
     return subjectList
 
 
+def p_prob_syntax_error_shortened():
+    problist=get_problems(df)
+    syntax_list=[]
+    sublist=list()
+    index=0
+
+    for x in range(len(problist)):
+
+        for a in range(len(df2)):
+            if df2['ProblemID'].iloc[a]==problist[x]:
+                if df2['SubjectID'].iloc[a] not in sublist:
+                    sublist.append((df2['SubjectID'].iloc[a]))
+                if(df2['CompileMessageType'].iloc[a]=='SyntaxError'):
+
+                    if df2['SubjectID'].iloc[a] not in syntax_list:
+                        syntax_list.append(df2['SubjectID'].iloc[a])
+                        index+=1
+
+        print('Problem ID'+ str(problist[x]))
+        print('No. of students who answered', len(sublist))
+        print('No of students who made syntax error', len(syntax_list))
+        print('Percentage of synatx error by subjects', len(syntax_list)/len(sublist))
+        sublist.clear()
+        syntax_list.clear()
+
+
+
+
+''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
 def p_prob_syntax_error():
     subjectID = getSubjects(df)
     problemList = get_problems(df)
@@ -34,7 +63,7 @@ def p_prob_syntax_error():
     # l = []
 
     for problemID in problemList:
-        for subject in subjectID:
+       for subject in subjectID:
             for column, rows in df2.iterrows():
                 if rows['SubjectID'] == subject and rows['ProblemID'] == problemID:
                     if rows['CompileMessageType'] == 'SyntaxError':
@@ -45,11 +74,11 @@ def p_prob_syntax_error():
     # df3 = pd.DataFrame(l,columns=problemList)
     # df3.to_csv('pProblemSyntaxError.csv', index=False)
     return dic
-
+'''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
 
 def main():
-    print(p_prob_syntax_error())
-
+    #print(p_prob_syntax_error())
+    p_prob_syntax_error_shortened()
 
 if __name__ == "__main__":
     main()
